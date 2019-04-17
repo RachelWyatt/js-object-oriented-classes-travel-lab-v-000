c2c48d9ebb506e86c25c1493fa26f0e02c1f101d
Rachel Wyatt <rw126@evansville.edu>
2019-04-17 18:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,26 @@ class Driver {
   }
 }
 class Route {
-  const beginningLocation = {
-    
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
   }
-  constructor (beginningLocation, endingLocation){
-    
+
+  blocksTravelled() {
+    const avenues = [
+      "1st Avenue",
+      "2nd Avenue",
+      "3rd Avenue",
+      "Lexington Avenue",
+      "Park",
+      "Madison Avenue",
+      "5th Avenue"
+    ];
+    const vertical = Math.abs(this.end.vertical - this.start.vertical);
+    const horizontal = Math.abs(
+      avenues.indexOf(this.end.horizontal) -
+        avenues.indexOf(this.start.horizontal)
+    );
+    return vertical + horizontal;
   }
-  blocksTravelled () {
-    
-  }
+}
