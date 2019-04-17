540b923387688ff90bf300cce221b98f0424c419
Rachel Wyatt <rw126@evansville.edu>
2019-04-17 18:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Driver {
   }
   
   yearsExperienceFromBeginningOf(year) {
-    startYear = this.startDate.getFullYear();
+    let startYear = this.startDate.getFullYear();
     return (year - startYear);
   }
 }
